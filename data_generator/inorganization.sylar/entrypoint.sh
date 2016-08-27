22394498f0dc932c356bc23705a5428eebce9d74
SylarChen <chenheng415@163.com>
2016-08-27 16:58:59 +0800

    Update entrypoint.sh

--- data_generator/inorganization.sylar/entrypoint.sh
+++ data_generator/inorganization.sylar/entrypoint.sh
@@ -2,10 +2,11 @@
 printenv | cat - /etc/cron.d/myCrontab > /root/crontab.temp && mv /root/crontab.temp /etc/cron.d/myCrontab
 #crontab file must be end of a new line
 sed -i -e '$a\' /etc/cron.d/myCrontab
-
+#start cron service
 service cron start
+#using this loop to make container live
 while true
 do
 echo "check..."
-sleep 10
+sleep 100
 done
